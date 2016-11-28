39db5f4d005c037e67ddc3dc901092c9bfcf3717
GalanEvgen <e.galan@i.ua>
2016-11-28 17:38:38 +0200

    'newFiles'

--- Angular2Start/app/main.ts
+++ Angular2Start/app/main.ts
@@ -1,22 +1,5 @@
-import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HelloAngularModule } from './app.module';
 
-@Component({
-	selector: 'hello-angular',
-	template: `
-		<div>
-			Hello Angular-2!
-		</div>
-	`
-})
-class HelloAngular{}
-
-@NgModule({
-	imports: [ BrowserModule ],
-	declarations: [ HelloAngular ],
-	bootstrap: [ HelloAngular ]
-})
-class HelloAngularModule{}
 
 platformBrowserDynamic().bootstrapModule(HelloAngularModule);
